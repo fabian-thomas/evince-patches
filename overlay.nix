65268f4e4d9e45648a660519ad6c21538387ebc8
Fabian Thomas <fabian@fabianthomas.de>
2024-03-18 23:04:15 +0100

    simplify overlay

--- overlay.nix
+++ overlay.nix
@@ -2,23 +2,18 @@
   evince = prev.evince.overrideAttrs (old: rec {
     version = old.version + "-patched";
 
-    patchesSrc = prev.fetchFromGitHub {
-      owner = "fabian-thomas";
-      repo = "evince-patches";
-      rev = old.version;
-      sha256 = "sha256-zECnPjTCv4EBt/0Al9B8+/lCIGrX3E6Jla/voxaw3YQ=";
-    };
+    patchesSrc = ./patches;
 
     patches = (old.patches or []) ++ [
-      "${patchesSrc}/patches/0-vim-like-bindings.patch"
-      "${patchesSrc}/patches/add-annotation-right-click.patch"
-      "${patchesSrc}/patches/add-keyboard-binding-to-toggle-menu-bar.patch"
-      "${patchesSrc}/patches/add-save-action-and-make-default.patch"
-      "${patchesSrc}/patches/add-web-search-action.patch"
-      "${patchesSrc}/patches/custom-commands.patch"
-      "${patchesSrc}/patches/initially-hide-menu-bar.patch"
-      # "${patchesSrc}/patches/sync-page.patch"
-      "${patchesSrc}/patches/zathura-like.patch"
+      "${patchesSrc}/0-vim-like-bindings.patch"
+      "${patchesSrc}/add-annotation-right-click.patch"
+      "${patchesSrc}/add-keyboard-binding-to-toggle-menu-bar.patch"
+      "${patchesSrc}/add-save-action-and-make-default.patch"
+      "${patchesSrc}/add-web-search-action.patch"
+      "${patchesSrc}/custom-commands.patch"
+      "${patchesSrc}/initially-hide-menu-bar.patch"
+      # "${patchesSrc}/sync-page.patch"
+      "${patchesSrc}/zathura-like.patch"
     ];
 
     # If applying the patches fails try with a higher fuzz value
